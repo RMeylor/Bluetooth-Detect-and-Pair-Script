80a2eddad768f4f74b07bcb31110160febfc93ca
RMeylor <112234783+RMeylor@users.noreply.github.com>
2022-08-27 10:33:33 -0700

    Add files via upload

---
+++ Bluetooth detect and pair script/Pair.ps1
@@ -0,0 +1,8 @@
+﻿$DeviceName = "Jabra Evolve 65"
+
+"Unpairing device: $DeviceName"
+btpair -u -n $DeviceName
+
+"Pairing device: $DeviceName"
+"Note: you may need to click on a Windows prompt and accept the PIN"
+btpair -p -n $DeviceName
